7ffa44d13f4bde2ab988a3517ef04fce1a9c40e4
Matthieu Brouillard <matthieu.brouillard@aaqua.live>
2021-12-21 20:00:36 +0100

    feat: add map, list + hardcoded data

---
+++ screens/Map.tsx
@@ -0,0 +1,54 @@
+import { Image, StyleSheet, Dimensions } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+// bxl
+const initialRegion = {
+  latitude: 50.850340,
+  longitude: 4.351710,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}
+
+export const ilClassico = {
+  coordinates: {
+    latitude: 50.82306517540965,
+    longitude: 4.380942028807865
+  },
+  name: 'My Tannour',
+  description: 'Une pizza achetee, une offerte!'
+}
+
+export default function Map({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <MapView initialRegion={initialRegion} style={styles.map}>
+        <Marker coordinate={ilClassico.coordinates} title={ilClassico.name} description={ilClassico.description} />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 32
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import { ilClassico } from './Map';
+
+const renderItem = ({ item }) => (
+  <View>
+    <Text>{item.name}</Text>
+  </View>
+)
+
+const keyExtractor = item => item.name
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <FlatList
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        data={[{...ilClassico}]}
+      />
     </View>
   );
 }
