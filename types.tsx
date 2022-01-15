58388ddaf217cfcee5cc4f9cd4da0ebbb47e12c1
Matthieu <matthieu@Matthieu-Denizs-MacBook-Pro.local>
2022-01-15 15:45:27 +0100

    chore: run eslint

--- types.tsx
+++ types.tsx
@@ -9,12 +9,12 @@ import {
   NavigatorScreenParams,
 } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
-}
+/*
+ * declare global {
+ *   namespace ReactNavigation {
+ *     type RootParamList = RootStackParamList
+ *   }
+ * } */
 
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
